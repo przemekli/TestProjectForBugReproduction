934f34f8acd08122482772a445fa5910231ef600
Przemek <thisisjustspaminfo@thisisjustspaminfo.com>
2017-09-20 11:03:30 +0200

    Adds project to the github

---
+++ ProjectWithPageObjects/ProjectWithPageObjectsTests/ExampleTests.m
@@ -0,0 +1,14 @@
+#import <XCTest/XCTest.h>
+#import "ProjectWithPageObjectsTests-Swift.h"
+
+@interface ExampleTests : XCTestCase
+
+@end
+
+@implementation ExampleTests
+
+- (void)testExample {
+    SwiftHelper *helper = [[SwiftHelper alloc] init];
+}
+
+@end
